9daf4a96ae32a3f9371cd8ceab131c2845cbb6cf
daz079 <38755167+daz079@users.noreply.github.com>
2022-09-09 10:25:08 +0100

    Installed sass and set it up

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+module.exports = eleventyConfig => {
+  eleventyConfig.addWatchTarget('./src/sass');
+  eleventyConfig.addPassthroughCopy('./src/css');
+  // eleventyConfig.addPassthroughCopy('./src/js');
+  // eleventyConfig.addPassthroughCopy('./src/img');
+  // eleventyConfig.addPassthroughCopy('./src/fonts');
+  // eleventyConfig.addPassthroughCopy('./src/svg');
+
+  return {
+    // templateFormats: ["md", "njk"],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    passthroughFileCopy: true,
+
+    dir: {
+      input: 'src',
+      output: 'public'
+    }
+  };
+};
