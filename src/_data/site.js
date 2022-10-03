83c966520c1219bb54e06921c28e4d7f0f2272f9
daz079 <38755167+daz079@users.noreply.github.com>
2022-10-03 10:33:11 +0100

    Added social share icons

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
   'siteName': 'Make Things Accessible',
-  'siteUrl': 'www.makethingsaccessible.com',
+  'siteUrl': 'https://www.makethingsaccessible.com',
   'version': '1.0',
   'year': new Date().getFullYear(),
   'siteEmail': 'test@test.com'
---
+++ src/guides/test-ben.md
@@ -0,0 +1,9 @@
+---
+title: Test Ben
+summary: Some test
+author: bwatson
+date: 2022-10-02
+tags:
+  - CSS
+---
+Some test
