56df998ad74769941bc3bc0b7e41e39be02db216
daz079 <38755167+daz079@users.noreply.github.com>
2022-10-04 16:05:50 +0100

    config

--- .eleventy.js
+++ .eleventy.js
@@ -20,7 +20,7 @@ module.exports = eleventyConfig => {
   const markdownItEleventyImgConfig = {
     imgOptions: {
       widths: [1200, 900, 600, 300],
-      urlPath: "/img/",
+      urlPath: "/guides/images/",
       outputDir: "./public/img/",
       formats: ["webp", "png"]
     },
